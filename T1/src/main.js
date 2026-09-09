eaff364ca4253cb6990e028dae5383bbcf7b11c6
Isadora Alexandre <isadoraalexandre489@gmail.com>
2026-09-09 20:37:22 -0300

    adicioana arquivos de colisao funcionado

--- T1/src/main.js
+++ T1/src/main.js
@@ -3,7 +3,6 @@ import { OrbitControls } from '../../build/jsm/controls/OrbitControls.js';
 import { PointerLockControls } from '../../build/jsm/controls/PointerLockControls.js';
 import { Octree } from '../../build/jsm/math/Octree.js';
 import { OctreeHelper } from '../../build/jsm/helpers/OctreeHelper.js';
-import { Capsule } from '../../build/jsm/math/Capsule.js';
 
 import {initRenderer, 
         initCamera,
@@ -14,6 +13,8 @@ import {initRenderer,
         createGroundPlaneXZ} from "../../libs/util/util.js";
 
 import { Castle } from './models/castle/Castle.js'
+import { PlayerController } from './player/PlayerController.js';
+import { PlayerPhysics } from './player/PlayerPhysics.js';
 
 
 // ------------------------ Initial variables ------------------------
@@ -55,56 +56,20 @@ scene.add(castle.object)
 castle.showBoundingBox(scene);
 
 
-// ------------------------ CAMERA ------------------------
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-let moveUp = false;
-let moveDown = false;
-
 // ------------------------ COLLISION ------------------------
 
 const worldOctree = new Octree();
 worldOctree.fromGraphNode(scene)
+const player = new PlayerController();
+const physics = new PlayerPhysics(worldOctree);
 
-const GRAVITY = 30
 const STEPS_PER_FRAME = 5
-const playerCollider = new Capsule(new THREE.Vector3( 0, 0.35, 0 ), new THREE.Vector3( 0, 1, 0 ), 0.35 );
-
-const playerVelocity = new THREE.Vector3();
-const playerDirection = new THREE.Vector3();
-
-let playerOnFloor = false;
-let mouseTime = 0;
-
-const keyStates = {};
-
-const vector1 = new THREE.Vector3();
-const vector2 = new THREE.Vector3();
-const vector3 = new THREE.Vector3();
-
-
-// Use this to show information onscreen
-// let controls = new InfoBox();
-//   controls.add("Basic Scene");
-//   controls.addParagraph();
-//   controls.add("Use mouse to interact:");
-//   controls.add("* Left button to rotate");
-//   controls.add("* Right button to translate (pan)");
-//   controls.add("* Scroll to zoom in/out.");
-//   controls.show();
 
 
 
 //  ------------------------ LISTENERS ------------------------
 
-document.body.addEventListener('click', function () {
-    controls.lock();
-});
-
-window.addEventListener('keydown', (event) => movementControls(event.keyCode, true));
-window.addEventListener('keyup', (event) => movementControls(event.keyCode, false));
+document.body.addEventListener('click', function() {controls.lock();});
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
 
 render();
@@ -112,128 +77,29 @@ render();
 
 //  ------------------------ FUNCTIONS ------------------------
 
-function playerCollisions(){
-
-    playerOnFloor = false;
-
-    for(let i = 0; i < 3; i++){
-        const result = worldOctree.capsuleIntersect(playerCollider);
-        if(!result) break;
-
-        if(result.normal.y >= 0.15){
-            playerOnFloor = true;
-        } else {
-            playerVelocity.addScaledVector(result.normal, -result.normal.dot(playerVelocity));
-        }
-
-        if(result.depth >= 1e-10){
-            playerCollider.translate(result.normal.multiplyScalar(result.depth));
-        }
-    }
-}
-
-function updatePlayer(deltaTime){
-    let damping = Math.exp(-4*deltaTime) - 1;
-
-    if(!playerOnFloor){
-        playerVelocity.y -= GRAVITY * deltaTime;
-        damping *= 0.1 //resitencia ao ar
-    }
-
-    playerVelocity.addScaledVector(playerVelocity, damping);
-
-    const deltaPosition = playerVelocity.clone().multiplyScalar(deltaTime);
-    playerCollider.translate(deltaPosition);
-
-    playerCollisions();
-
-    camera.position.copy(playerCollider.end);
-
-}
-
-function getForwardVector(){
-    camera.getWorldDirection(playerDirection);
-    playerDirection.y =0
-    playerDirection.normalize();
-
-    return playerDirection;
-}
-
-function getSideVector(){
-    camera.getWorldDirection(playerDirection);
-    playerDirection.y =0
-    playerDirection.normalize();
-    playerDirection.cross(camera.up)
-
-    return playerDirection;
-}
-
-function movementControls(key, value) {
-    switch (key) {
-        case 38:
-        case 87: // W
-            moveForward = value;
-            break;
-        
-        case 40:
-        case 83: // S
-            moveBackward = value;
-            break;
-
-        case 37:
-        case 65: // A
-            moveLeft = value;
-            break;
-        
-        case 39:
-        case 68: // D
-            moveRight = value;
-            break;
-        case 32: //space
-            moveUp = value;
-            break;
-        case 16: // shift
-            moveDown = value;
-            break;
-    }
-}
-
-
 function moveControls(deltaTime){
-    const speedDelta = deltaTime * (playerOnFloor ? 100 : 70)
+    const speedDelta = deltaTime * (player.playerOnFloor ? 100 : 50)
 
-    if(moveForward){
-        playerVelocity.add(getForwardVector().multiplyScalar(speedDelta))
+    if(player.moveForward){
+        physics.playerVelocity.add(physics.getForwardVector(camera).multiplyScalar(speedDelta))
     }
-    if(moveBackward){
-        playerVelocity.add(getForwardVector().multiplyScalar(-speedDelta))
-    }
-
-    if(moveLeft){
-        playerVelocity.add(getSideVector().multiplyScalar(-speedDelta))
-    }
-    if(moveRight){
-        playerVelocity.add(getSideVector().multiplyScalar(speedDelta))
+    if(player.moveBackward){
+        physics.playerVelocity.add(physics.getForwardVector(camera).multiplyScalar(-speedDelta))
     }
 
-    if(playerOnFloor){
-        if(moveUp){
-            playerVelocity.y = 70;
-        }
+    if(player.moveLeft){
+        physics.playerVelocity.add(physics.getSideVector(camera).multiplyScalar(-speedDelta))
     }
-}
+    if(player.moveRight){
+        physics.playerVelocity.add(physics.getSideVector(camera).multiplyScalar(speedDelta))
+    }
 
-
-
-function teleportPlayerIfOob() {
-	if ( camera.position.y <= - 25 ) {
-		playerCollider.start.set( 0, 0.35, 0 );
-		playerCollider.end.set( 0, 1, 0 );
-		playerCollider.radius = 0.35;
-		camera.position.copy( playerCollider.end );
-		camera.rotation.set( 0, 0, 0 );
+    if(physics.playerOnFloor){
+        if (player.moveUp)
+            physics.playerVelocity.y = 40;
     }
 }
+
 
 
 
@@ -246,11 +112,22 @@ function render()
     for(let i=0; i< STEPS_PER_FRAME; i++){
         if(controls.isLocked){
             moveControls(deltaTime)
-            updatePlayer(deltaTime)
+            physics.updatePlayer(deltaTime)
+            camera.position.copy(physics.playerCollider.end);
         }
-        teleportPlayerIfOob();
+        physics.teleportPlayerIfOob(camera);
     }
 
     renderer.render(scene, camera)
     requestAnimationFrame(render);
 }
+
+// Use this to show information onscreen
+// let controls = new InfoBox();
+//   controls.add("Basic Scene");
+//   controls.addParagraph();
+//   controls.add("Use mouse to interact:");
+//   controls.add("* Left button to rotate");
+//   controls.add("* Right button to translate (pan)");
+//   controls.add("* Scroll to zoom in/out.");
+//   controls.show();
---
+++ T1/src/player/PlayerPhysics.js
@@ -0,0 +1,79 @@
+import * as THREE from  'three';
+import { Capsule } from '../../../build/jsm/math/Capsule.js';
+
+
+const GRAVITY = 30
+
+export class PlayerPhysics{
+    constructor(worldOctree){
+        this.worldOctree = worldOctree;
+        this.playerCollider = new Capsule(new THREE.Vector3( 0, 0.35, 0 ), new THREE.Vector3( 0, 1, 0 ), 0.35 );
+        this.playerVelocity = new THREE.Vector3();
+        this.playerDirection = new THREE.Vector3();
+        this.playerOnFloor = false;
+    }
+
+    playerCollisions(){
+        this.playerOnFloor = false;
+        const result = this.worldOctree.capsuleIntersect(this.playerCollider);
+
+        if(result){
+            this.playerOnFloor = result.normal.y >= 0.15;
+
+            if(!this.playerOnFloor){
+                this.playerVelocity.addScaledVector(result.normal, -result.normal.dot(this.playerVelocity));
+            }
+
+            if(result.depth >= 1e-10){
+                this.playerCollider.translate(result.normal.multiplyScalar(result.depth));
+            }
+        }
+        
+    }
+
+    updatePlayer(deltaTime){
+        let damping = Math.exp(-4*deltaTime) - 1;
+
+        if(!this.playerOnFloor){
+            this.playerVelocity.y -= GRAVITY * deltaTime;
+            damping *= 0.1 //resitencia ao ar
+        }
+
+        this.playerVelocity.addScaledVector(this.playerVelocity, damping);
+
+        const deltaPosition = this.playerVelocity.clone().multiplyScalar(deltaTime);
+        this.playerCollider.translate(deltaPosition);
+
+        this.playerCollisions();
+
+        //camera.position.copy(playerCollider.end);
+
+    }
+
+    getForwardVector(camera){
+        camera.getWorldDirection(this.playerDirection);
+        this.playerDirection.y =0
+        this.playerDirection.normalize();
+
+        return this.playerDirection;
+    }
+
+    getSideVector(camera){
+        camera.getWorldDirection(this.playerDirection);
+        this.playerDirection.y =0
+        this.playerDirection.normalize();
+        this.playerDirection.cross(camera.up)
+
+        return this.playerDirection;
+    }
+
+    teleportPlayerIfOob(camera) {
+        if ( camera.position.y <= - 25 ) {
+            this.playerCollider.start.set( 0, 0.35, 0 );
+            this.playerCollider.end.set( 0, 1, 0 );
+            this.playerCollider.radius = 0.35;
+            camera.position.copy( this.playerCollider.end );
+            camera.rotation.set( 0, 0, 0 );
+        }
+    }
+}
