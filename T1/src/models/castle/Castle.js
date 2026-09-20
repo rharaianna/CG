1ce04db4dd05e560185e24bfede8d097058938ac
João Pedro <103667165+martins-joaopedro@users.noreply.github.com>
2026-09-20 16:33:44 -0300

    fix: corrige problema do posicionamento da parede detalhada

--- T1/src/models/castle/Castle.js
+++ T1/src/models/castle/Castle.js
@@ -244,7 +244,11 @@ export class Castle extends Model {
         const wall3 = new CastleWall(wallX, wallY, 0, this.material, DEPTH, WALL_HEIGHT, WALL_DEPTH, TOWER_RADIUS, doorWallConfig)
         
         // lado da parede com detalhes
-        const wall4 = new CastleWall(-wallX, wallY, -DEPTH/4, this.material, DEPTH/2, WALL_HEIGHT, WALL_DEPTH, TOWER_RADIUS/2, null)
+        const wall4 = new CastleWall(-wallX, wallY, -DEPTH/4, this.material, DEPTH/2 - MIDTOWER_WIDTH/2, WALL_HEIGHT, WALL_DEPTH, TOWER_RADIUS/2, null)
+        
+        // empurro ela pro local correto 
+        wall4.object.translateZ(-MIDTOWER_WIDTH/4)
+        
         const wall44 = new DetailedWall(-wallX, wallY, DEPTH/4, this.material, DEPTH/2, WALL_HEIGHT, WALL_DEPTH, MIDTOWER_WIDTH, MIDTOWER_DEPTH, null)
         
         wall3.object.rotateY(THREE.MathUtils.degToRad(90))
